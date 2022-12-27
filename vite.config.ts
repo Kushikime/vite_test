2ade01e9b604e495f053b69c4ddedb85d5c76e95
erik.demchak@dormakaba-external.com <erik.demchak@dormakaba-external.com>
2022-12-27 21:33:12 +0100

    Added .env.development .env.production
    added an empty Jenkinsfile
    removed unused react default files/folders

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  root: './', // where is the index.html of the project.
+  base: './', // where is the public folder located.
   plugins: [react()],
+  server: {
+    port: 3000,
+  },
 })
